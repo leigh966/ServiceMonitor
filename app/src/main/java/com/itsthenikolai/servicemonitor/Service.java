b253590f8c69ce1d5d134f33757fedc378fae3a4
Leigh966 <leigh966@hotmail.com>
2024-05-15 13:52:05 +0100

    removed unused imports

--- app/src/main/java/com/itsthenikolai/servicemonitor/Service.java
+++ app/src/main/java/com/itsthenikolai/servicemonitor/Service.java
@@ -1,11 +1,8 @@
 package com.itsthenikolai.servicemonitor;
 
 import androidx.annotation.NonNull;
-import androidx.media3.common.Format;
 import androidx.room.ColumnInfo;
-import androidx.room.Embedded;
 import androidx.room.Entity;
-import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 @Entity
